c20a31da5c6c035e809410e7333961163aeec14c
iwilliams001 <isaacwilliams1738@gmail.com>
2024-07-14 18:41:46 -0400

    Added comments to cypher.py

--- Day08/cypher.py
+++ Day08/cypher.py
@@ -1,8 +1,10 @@
-alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+# Encode message
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
+            'v', 'w', 'x', 'y', 'z']
 
 
-#Create a function called 'encrypt' that takes the 'text' and 'shift' as inputs.
-def caesar(text,shift,direction):
+# Create a function called 'encrypt' that takes the 'text' and 'shift' as inputs.
+def caesar(text, shift, direction):
     cipher_text = ""
     for letter in text:
         if letter in alphabet:
@@ -23,5 +25,5 @@ while restart == "y":
     direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
     text = input("Type your message:\n").lower()
     shift = int(input("Type the shift number:\n"))
-    caesar(text,shift,direction)
+    caesar(text, shift, direction)
     restart = input("Do you want to decipher another message? Yes or No?:\n")[0].lower()
